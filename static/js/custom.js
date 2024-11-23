e84ad76b7c755751df775eed7e4a08b23df62516
hunger188 <860750030@qq.com>
2024-11-23 14:41:56 +0800

    前端添加了一点动效

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,13 +1,19 @@
 $(document).ready(function () {
+
+    $('.loader').hide();
     // 提问部分
-    $('#ask-btn').click(function () {
+    $('#search-botton').click(function () {
         const question = $('#question').val().trim();
 
+        $('#answer').addClass('d-none');
+
         if (!question) {
-            alert('请输入问题');
+            //alert('请输入问题');
+            $('#QAModal').modal()
             return;
         }
 
+        $('.loader').show();
         // 向后端发送问题请求
         $.ajax({
             url: '/ask',
@@ -16,23 +22,28 @@ $(document).ready(function () {
             data: JSON.stringify({ question: question }),
             success: function (data) {
                 //使用 .html() 方法渲染 HTML 内容，这样 <br> 标签才会生效
+                $('.loader').hide();
                 $('#answer').removeClass('d-none').html(data.answer);
             },
             error: function () {
+                $('.loader').hide();
                 $('#answer').removeClass('alert-success').addClass('alert-danger').html('发生错误，请稍后重试');
             }
         });
     });
+    //
+
 
     // 生成知识图谱部分
-    $('#generate-btn').click(function () {
+    $('#generate-botton').click(function () {
         const cypherQuery = $('#query').val().trim();
 
+        $('#graph-frame').addClass('d-none');
         if (!cypherQuery) {
-            alert('请输入查询');
+            $('#GENModal').modal();
             return;
         }
-
+        $('.loader').show();
         // 向后端发送生成图谱请求
         $.ajax({
             url: '/generate_graph',
@@ -41,8 +52,11 @@ $(document).ready(function () {
             data: JSON.stringify({ query: cypherQuery }),
             success: function (data) {
                 if (data.graph_url) {
+                    $('.loader').hide();
                     $('#graph-frame').removeClass('d-none').attr('src', data.graph_url);
                 } else {
+                    $('.loader').hide();
+                    $('#GEN-ERROR-Modal').modal()
                     alert(data.error || '生成图谱失败');
                 }
             },
